9ea6906f2679c1e475f9c5740c71d2db13b6da25
Adam Goodchild <mimminito.ag@gmail.com>
2019-07-07 23:25:26 +0100

    Renamed Player Died event to Life Lost event. Added in PlayerBaseManager. This spawns the trophy when the level starts. Updated Base-Trophy prefab to give it a Health component. This allows projectiles to damage the trophy and kill it, which results in the player losing a life.

---
+++ Assets/Scripts/Managers/PlayerBaseManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityTankBattalion
+{
+    public class PlayerBaseManager : MonoBehaviour
+    {
+        #region Public Variables
+
+        /// <summary>
+        /// The trophy prefab
+        /// </summary>
+        public GameObject BaseTrophy;
+
+        /// <summary>
+        /// The trophy spawn point
+        /// </summary>
+        public Transform SpawnPoint;
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Called when the level starts
+        /// </summary>
+        public void OnLevelStart()
+        {
+            // Spawn the trophy
+            Pooling.GetFromPool(BaseTrophy, SpawnPoint.position, Quaternion.identity);
+        }
+
+        #endregion
+    }
+}
