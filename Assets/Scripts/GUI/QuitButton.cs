8941ba026790a4f36c330d772de0afb2ac3f9d6d
Adam Goodchild <mimminito.ag@gmail.com>
2019-07-08 16:57:52 +0100

    Added in QuitButton. This will quit the application when pressed. It will also stop the editor playing if running inside the editor.

---
+++ Assets/Scripts/GUI/QuitButton.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityTankBattalion
+{
+    public class QuitButton : MonoBehaviour
+    {
+        #region Unity Methods
+
+        private void Start()
+        {
+            Button button = GetComponent<Button>();
+            if (!button)
+            {
+                return;
+            }
+
+            button.onClick.AddListener(QuitApp);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void QuitApp()
+        {
+            if (Application.isEditor && Application.isPlaying)
+            {
+#if UNITY_EDITOR
+                EditorApplication.isPlaying = false;
+#endif
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+
+        #endregion
+    }
+}
