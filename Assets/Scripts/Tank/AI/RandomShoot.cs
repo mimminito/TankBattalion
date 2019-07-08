6c7ac676bfe501774e9aaa04d45dd7a1d52ba59a
Adam Goodchild <mimminito.ag@gmail.com>
2019-07-08 09:42:42 +0100

    Organised the Tank scripts a little better. Added in RandomShoot class. This randomises a timer and then fires a weapon, and repeats this process again. Added in SimpleEnemyTankInputController class. This randomises a timer and then randomises the input for the tank. It constantly updates the input every frame. This is a very simple AI however and has no knowledge of its surroundings. Updated TankEnemy prefab with RandomShoot and SimpleEnemyTankInputController components. Updated TankEnemy and TankPlayer prefabs to have 10 starting health so they get killed in one shot.

---
+++ Assets/Scripts/Tank/AI/RandomShoot.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+using Random = UnityEngine.Random;
+
+namespace UnityTankBattalion
+{
+    [RequireComponent(typeof(TankWeapon))]
+    public class RandomShoot : MonoBehaviour
+    {
+        #region Public Variables
+
+        /// <summary>
+        /// The minimum amount of time that can pass before we randomise shooting
+        /// </summary>
+        public float MinFrequency = 3f;
+
+        /// <summary>
+        /// The maximum amount of time that can pass before we randomise shooting
+        /// </summary>
+        public float MaxFrequency = 5f;
+
+        #endregion
+
+        #region Private Variables
+
+        /// <summary>
+        /// Our timer
+        /// </summary>
+        private float mTimer;
+
+        /// <summary>
+        /// Our weapon
+        /// </summary>
+        private TankWeapon mTankWeapon;
+
+        #endregion
+
+        #region Unity Methods
+
+        private void Awake()
+        {
+            // Initialise this component
+            Init();
+        }
+
+        private void Update()
+        {
+            // Update our timer
+            UpdateTimer();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Initialise this component
+        /// </summary>
+        private void Init()
+        {
+            // Grab our weapon
+            mTankWeapon = GetComponent<TankWeapon>();
+
+            // If we do not have a weapon attached then return out
+            if (!mTankWeapon)
+            {
+                return;
+            }
+
+            DetermineNextFrequency();
+        }
+
+        /// <summary>
+        /// Updates our timer
+        /// </summary>
+        private void UpdateTimer()
+        {
+            // Update our timer
+            mTimer -= Time.deltaTime;
+
+            // Check if our timer has elapsed
+            if (mTimer <= 0f)
+            {
+                // Randomise our timer
+                DetermineNextFrequency();
+
+                // Decide on some movement
+                FireWeapon();
+            }
+        }
+
+        /// <summary>
+        /// Fires our weapon
+        /// </summary>
+        /// <returns></returns>
+        private void FireWeapon()
+        {
+            // Check we have a weapon
+            if (!mTankWeapon)
+            {
+                return;
+            }
+
+            // Fire the weapon
+            mTankWeapon.FireWeapon();
+        }
+
+        /// <summary>
+        /// Determines our next frequency value for the timer
+        /// </summary>
+        /// <returns></returns>
+        private void DetermineNextFrequency()
+        {
+            mTimer = Random.Range(MinFrequency, MaxFrequency);
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Tank/AI/SimpleEnemyTankInputController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace UnityTankBattalion
+{
+    public class SimpleEnemyTankInputController : MonoBehaviour
+    {
+        #region Public Variables
+
+        /// <summary>
+        /// The minimum amount of time that can pass before we randomise input
+        /// </summary>
+        public float MinFrequency = 3f;
+        
+        /// <summary>
+        /// The maximum amount of time that can pass before we randomise input
+        /// </summary>
+        public float MaxFrequency = 5f;
+
+        #endregion
+
+        #region Private Variables
+
+        /// <summary>
+        /// Our tank movement component
+        /// </summary>
+        private TankMovement mTankMovement;
+
+        /// <summary>
+        /// Timer for deciding our next command
+        /// </summary>
+        private float mTimer;
+
+        /// <summary>
+        /// Our current input 
+        /// </summary>
+        private Vector2 mCurrentInput;
+
+        #endregion
+
+        #region Unity Methods
+
+        private void Awake()
+        {
+            // Grab our component
+            mTankMovement = GetComponent<TankMovement>();
+
+            // Set our timer
+            DetermineNextFrequency();
+            
+            // Start with some random input
+            RandomiseInput();
+        }
+
+        private void Update()
+        {
+            // Updates the timer
+            UpdateTimer();
+            
+            // Applies our current input
+            ApplyInput();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Updates the timer until we can randomise a new movement
+        /// </summary>
+        private void UpdateTimer()
+        {
+            // Update our timer
+            mTimer -= Time.deltaTime;
+
+            // Check if our timer has elapsed
+            if (mTimer <= 0f)
+            {
+                // Randomise our timer
+                DetermineNextFrequency();
+
+                // Decide on some movement
+                RandomiseInput();
+            }
+        }
+
+        /// <summary>
+        /// Randomise our movement
+        /// </summary>
+        private void RandomiseInput()
+        {
+            int moveHorizontal = Random.Range(0, 2);
+
+            // If we are moving horizontal
+            if (moveHorizontal == 0)
+            {
+                int horizontalDirection = Random.Range(0, 2);
+                mCurrentInput = new Vector2(horizontalDirection == 0 ? -1f : 1f, 0f);
+            }
+            else
+            {
+                int verticalMovement = Random.Range(0, 2);
+                mCurrentInput = new Vector2(0f, verticalMovement == 0 ? -1f : 1f);
+            }
+        }
+
+        /// <summary>
+        /// Applies the input to the TankMovement script
+        /// </summary>
+        private void ApplyInput()
+        {
+            mTankMovement.SetInput(mCurrentInput);
+        }
+
+        /// <summary>
+        /// Determines our next frequency value for the timer
+        /// </summary>
+        /// <returns></returns>
+        private void DetermineNextFrequency()
+        {
+            mTimer = Random.Range(MinFrequency, MaxFrequency);
+        }
+
+        #endregion
+    }
+}
